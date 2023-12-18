7dfbb652719f4d28452fdbd266883930eba63820
Adam Adnan <echo.adnan100701@gmail.com>
2023-12-18 23:10:58 +0700

    first commit

---
+++ app/views/home/index.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="jumborton mt-4">
+        <h1 class="display-4">
+            Selamat Datang di website saya!
+        </h1>
+        <p class="lead">
+            Halo, nama saya <?= $data['nama']; ?>
+        </p>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id nihil ea rem natus nostrum debitis eius, tempora eum mollitia fuga expedita impedit ut error non aliquid quae iste tenetur quod reiciendis molestiae molestias, quidem ex excepturi? Sequi fugit officia ullam, obcaecati quaerat omnis sapiente magni necessitatibus id excepturi facilis repellendus?</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
